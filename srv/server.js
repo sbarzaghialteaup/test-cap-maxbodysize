72f77ab82fab4aa2321937602fd0d374b2f9c7bf
Samuele Barzaghi <sbarzaghi@alteanet.it>
2022-05-13 19:26:58 +0200

    Temporary workaround from sap support Incident 291152/2022

--- srv/server.js
+++ srv/server.js
@@ -1,6 +1,9 @@
 const cds = require("@sap/cds");
+const express = require("express");
 
 cds.on("bootstrap", (app) => {
+  // Workaround Incident 291152/2022
+  app.use(express.json({ limit: "10MB" }));
   cds.serve("CatalogService").in(app).to("rest").at("rest");
 });
 
